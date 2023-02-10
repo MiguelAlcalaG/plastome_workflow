6ef63066da669cf16dec31612008a20738187565
MiguelAlcalaG <108360354+MiguelAlcalaG@users.noreply.github.com>
2023-02-10 13:59:04 -0600

    Update zipextract_fastqc_script.sh

--- verify_cleaning/zipextract_fastqc_script.sh
+++ verify_cleaning/zipextract_fastqc_script.sh
@@ -2,5 +2,5 @@
 while read name;
 	do
 	echo $name >> results.txt
-	unzip -p Epidendrum_$name/fqc_$name/Epidendrum_$name\*_paired_fastqc.zip Epidendrum_$name\*/summary.txt | awk '{print $1}' >> results.txt;
+	unzip -p $name/fqc_$name/$name\*_paired_fastqc.zip $name\*/summary.txt | awk '{print $1}' >> results.txt;
 done < namelist3.txt
