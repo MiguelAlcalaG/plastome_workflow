0c0bcc62046cb8730a8912256a2dc9ab42bfefb4
MiguelAlcalaG <108360354+MiguelAlcalaG@users.noreply.github.com>
2023-02-27 12:53:24 -0600

    Add files via upload

---
+++ auxiliary_notes/compare_annotations.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[26]:
+
+
+import os
+import subprocess
+
+print("This script compares two plastome annotations from two .tsv files")
+
+os.chdir("/home/epidendrum/Downloads")
+
+nameoffile1 = input()
+nameoffile2 = input()
+
+command1 = "awk '{print $1}' " + nameoffile1 + " > file1.txt"
+command2 = "awk '{print $1}' " + nameoffile2 + " > file2.txt"
+
+subprocess.call(command1, shell=True)
+subprocess.call(command2, shell=True)
+
+file1 = open("file1.txt", "r")
+file2 = open("file2.txt", "r")
+
+data1 = file1.read()
+data2 = file2.read()
+
+list1 = data1.split("\n")
+list2 = data2.split("\n")
+
+print("Hace falta en el segundo archivo en referencia al primero")
+print(list(set(list1)-set(list2)))
+print("Hace falta en el primer archivo en referencia al segundo")
+print(list(set(list2)-set(list1)))
+
